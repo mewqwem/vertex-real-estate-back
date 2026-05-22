4a42752dfc691dfb4d6cfc8a389b49aa6e3c2c33
Oleh <roadto12mmr@gmail.com>
2026-05-22 12:27:53 +0200

    fix path to hotOffers w

--- src/routes/apartmentsRoutes.js
+++ src/routes/apartmentsRoutes.js
@@ -19,7 +19,7 @@ const router = Router();
 
 router.get('/apartments', celebrate(getApartmentsSchema), getAparmtents);
 
-router.get('/apartments/hotoffers', getHotOffers);
+router.get('/apartments/test/hotoffers', getHotOffers);
 
 router.get(
   '/apartments/:id',
